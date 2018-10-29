eec16585bc33fee182eec15903953e596d05756a
Jefferson Carvalho <jeffersoncarvalho60@outlook.com>
2018-10-29 12:07:51 -0300

    Updating Classes

--- src/estacionamento/EstacionamentoVO.java
+++ src/estacionamento/EstacionamentoVO.java
@@ -7,8 +7,119 @@ package estacionamento;
 
 /**
  *
- * @author acer
+ * @author Jefferson Carvalho
  */
 public class EstacionamentoVO {
     
-}
+    //atributos:
+    private int vagasDisponiveis, vagasOcupadas;
+    private int atual[] = new int[3];
+    private int total[] = new int[3];
+    private float precos[] = {4, 8, 10};
+
+    private int mapa[] = new int[64];
+
+    private String tipos[] = {"Moto", "Carro", "Van"};
+
+    private float totalCaixa;
+
+    //metodo construtor define o estado inicial de cada atributo:
+    public EstacionamentoVO() {
+
+        this.vagasDisponiveis = 64;
+        this.vagasOcupadas = 0;
+        this.totalCaixa = 0;
+
+        //zerando vetor: esvaziando vagas:
+        for (int i = 0; i < mapa.length; i++) {
+            this.mapa[i] = 0;
+        }//fechando for 
+
+        //zerando vetores das vagas e atuais:
+        for (int i = 0; i < 3; i++) {
+            atual[i] = 0;
+            total[i] = 0;
+        }//fechando for²
+
+    }//fechando construtor
+
+    //metodos funcionais:
+    public int adicionarVeiculo(int tmpTipo) {
+        int vagaDisp = -1;
+        boolean achou = false;
+
+        //algoritmo de busca da vaga disponivel:
+        for (int i = 0; i < this.mapa.length; i++) {
+            if (mapa[i] == 0) {
+                achou = true;//vaga encontrada
+                vagaDisp = i;//armazenando numero da vaga
+                this.mapa[vagaDisp] = tmpTipo;//estacionando veiculo
+                this.vagasDisponiveis--;
+                this.vagasOcupadas++;
+                this.atual[tmpTipo - 1]++;
+                break;//encerrando a varredura 
+            }//fechando if
+        }//fechando algoritmo
+        if (achou == false) {
+            return -1;
+        } else {
+            return vagaDisp;
+        }
+    }//fechando retorno da vaga;
+
+    public boolean retirarVeiculo(int tmpVaga) {
+        int tipo;
+
+        if (this.mapa[tmpVaga - 1] == 0) {
+            return false;
+        } else {
+            //retirar veiculo
+            
+            this.vagasDisponiveis++;
+            this.vagasOcupadas--;
+            tipo = this.mapa[tmpVaga -1];
+            this.atual[tipo-1]--;
+            this.total[tipo-1]++;
+
+            this.totalCaixa += precos[tipo-1];
+            this.mapa[tmpVaga -1] = 0;//tirando veiculo da vaga
+            
+           
+            return true;
+        }
+    }
+
+    public String visualizarMapa() {
+        String msgMapa = "";
+
+        for (int i = 0; i < this.mapa.length; i++) {
+            msgMapa += this.mapa[i] + " | ";
+            if (((i + 1) % 8 == 0) && (i != 0)) {
+                msgMapa += "\n";
+            }//fechando if 
+        }//fechando for
+        return msgMapa;
+    }//fechando visualizarMapa
+
+    public String[] getTipo() {
+        return tipos;
+    }
+
+    public String exibirRelatorio() {
+        String rel = "Veículos no estacionamento: \n";
+        for (int i = 0; i < atual.length; i++) {
+            rel += tipos[i] + ": " + atual[i] + "\n";
+        }
+        rel += "\nVagas Ocupadas: " + vagasOcupadas + "\n";
+        rel += "\nVagas Disponíveis: " + vagasDisponiveis + "\n";
+        rel += "Ganho por categoria: \n\n";
+
+        for (int i = 0; i < atual.length; i++) {
+            rel+=tipos[i] + " R$: " + (total[i]*precos[i]) + "\n";
+        }
+        rel += "\nTotal em caixa R$: " + totalCaixa + "\n";
+        
+        return rel;
+    }
+}//fechando classe
+
--- src/estacionamento/EstacionamentoView.java
+++ src/estacionamento/EstacionamentoView.java
@@ -1,14 +1,79 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package estacionamento;
+
+import javax.swing.*;
 
 /**
  *
- * @author acer
+ * @author Jefferson Carvalho
  */
 public class EstacionamentoView {
+    public static EstacionamentoVO objDados = new EstacionamentoVO();
     
+    public EstacionamentoView(){
+        
+        int opcao;
+        String msgMenu = "";
+        msgMenu += "Entre com a opção desejada: \n";
+        msgMenu +="1 - Visualizar mapa de vagas\n"; 
+        msgMenu +="2 - Entrada de veículos\n";
+        msgMenu +="3 - Saída de veículos\n";
+        msgMenu +="4 - Relatório\n";
+        msgMenu +="5 - Sair\n";
+       
+        do{
+        
+        opcao = Integer.parseInt(JOptionPane.showInputDialog(msgMenu));
+        
+        switch(opcao){
+            case 1:
+                //visualizar mapa
+                JOptionPane.showMessageDialog(null,objDados.visualizarMapa(),"CIC Park",JOptionPane.QUESTION_MESSAGE);
+                break;
+            case 2:
+                //registrarVeiculo();
+                registrarVeiculo();
+                break;
+            case 3:
+                //liberarVaga
+                liberarVaga();
+                break;
+            case 4:
+                //exibirRelatorio
+                JOptionPane.showMessageDialog(null, objDados.exibirRelatorio());
+                break;
+            case 5:
+                break;
+                
+            default:
+                JOptionPane.showMessageDialog(null, "Opção Inválida!","Estacionamento",JOptionPane.WARNING_MESSAGE); 
+        }//fechando switch
+        
+        }while(opcao!=5);
+    }  
+    public static void registrarVeiculo(){
+        int tmpTipo, numVaga;//numVaga armazena a vaga em que o veiculo está, armazena o retorno
+        tmpTipo = Integer.parseInt(JOptionPane.showInputDialog("Entre com o tipo do veículo:\n1 - Moto R$ 4\n2 - Carro R$ 8\n3 - Van R$ 10\n"));
+        
+        numVaga = objDados.adicionarVeiculo(tmpTipo);
+        
+        if(numVaga == -1){
+            JOptionPane.showMessageDialog(null, "Estacionamento lotado.","CIC Park",JOptionPane.ERROR_MESSAGE);
+        }else{
+            JOptionPane.showMessageDialog(null,"Seu(sua)" + objDados.getTipo()[tmpTipo -1] + " foi estacionado(a) na vaga: " + (numVaga + 1));
+        }
+    }
+    
+    public static void liberarVaga(){
+        boolean status;
+        int numVaga = Integer.parseInt(JOptionPane.showInputDialog("Entre com o número da vaga do cliente: "));
+        
+        status = objDados.retirarVeiculo(numVaga);
+        
+        if(status){ //mesma coisa que: if(status == true)
+            JOptionPane.showMessageDialog(null, "O veículo foi retirado da vaga: " + numVaga,"CIC Park",JOptionPane.PLAIN_MESSAGE);
+        }else{
+            JOptionPane.showMessageDialog(null, "Não há veículo a ser retirado","CIC Park",JOptionPane.ERROR_MESSAGE);
+        }
+    }
 }
